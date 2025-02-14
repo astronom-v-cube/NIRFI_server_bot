8f99635fc68de738537d6966b23186f0afaef5bf
Dmitry <astronom.v.cube@gmail.com>
2025-02-14 18:21:49 +0300

    chdir on server

--- bot.py
+++ bot.py
@@ -3,6 +3,12 @@
 
 from longpool import job_longpool
 import logging
+import os
+
+try:
+    os.chdir("D:\Dmitry\NIRFI_server_bot")
+except:
+    pass
 
 logging.basicConfig(filename = 'logs.log',  filemode='a', level = logging.INFO, format = ' %(asctime)s - %(levelname)s - %(message)s', encoding = "UTF-8", datefmt='%d-%b-%y %H:%M:%S')
 
