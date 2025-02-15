5202ac3b337d4eeadc46a538b9e1e2795b03679d
Dmitry <astronom.v.cube@gmail.com>
2025-02-15 16:41:32 +0300

    forse

--- bot.py
+++ bot.py
@@ -10,23 +10,8 @@ import os
 
 try:
     os.chdir("D:/Dmitry/NIRFI_server_bot")
-<<<<<<< HEAD
 except Error as err:
     logging.info(err)
-=======
-except:
-    logging.info('Не сменил')
-
-def ping_ip(ip):
-    param = '-n' if platform.system().lower() == 'windows' else '-c'
-    command = ['ping', param, '1', ip]
-    with open(os.devnull, 'w') as devnull:
-        if subprocess.call(command, stdout=devnull, stderr=devnull) == 0:
-            print(f"{ip} is active")
-
-ping_ip(f"10.0.0.137")
-
->>>>>>> 18c36a8c0dfde3ed7af5416892e99b0eedc2b5e8
 
 logging.basicConfig(filename = 'logs.log',  filemode='a', level = logging.INFO, format = ' %(asctime)s - %(levelname)s - %(message)s', encoding = "UTF-8", datefmt='%d-%b-%y %H:%M:%S')
 
